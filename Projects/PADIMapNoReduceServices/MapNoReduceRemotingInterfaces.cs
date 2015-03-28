6d64abdc3e9d39be26d882c1352b4ceb6c183ef2
AngeloPingo <angelopingosk8@gmail.com>
2015-03-28 19:56:37 +0000

    First commit

---
+++ Projects/PADIMapNoReduceServices/MapNoReduceRemotingInterfaces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PADIMapNoReduceServices
+{
+    public interface IPuppetMaster {
+        List<string> RegisterWorker(string NewWorkerPort);
+		List<string> RegisterClient(string NewClientPort);
+		void SubmitJob(File textFile);
+		void JodResult(string result);
+		//IList<KeyValuePair<string, string>> Map(string fileLine);
+	}
+
+	public interface IClient {
+        void SendResults(string result);
+	}
+	
+	public interface IWorker {
+		void DoJob(string fileSplited);
+	}
+}
---
+++ Projects/PuppetMaster/PuppetMaster.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Threading;
+using PADIMapNoReduceServices;
+
+namespace PuppetMaster
+{
+    public class PuppetMaster
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            TcpChannel channel = new TcpChannel(8086);
+            ChannelServices.RegisterChannel(channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                typeof(PuppetMasterServices), "PuppetMaster",
+                WellKnownObjectMode.Singleton);
+            System.Console.WriteLine("Press <enter> to terminate chat server...");
+            System.Console.ReadLine();
+        }
+    }
+
+    class PuppetMasterServices : MarshalByRefObject, IPuppetMaster
+    {
+        List<IWorker> clients;
+
+        PuppetMasterServices()
+        {
+            clients = new List<IWorker>();
+        }
+
+    }
+}
